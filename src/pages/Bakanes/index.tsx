f9ad7fd78315acd6e9d637970c1a05ae7e619dd6
Carlox <carlito1999@live.com>
2025-12-30 10:43:09 -0500

    feat: Implement new Bakanes page with tabbed navigation, a data table, search, filter, and pagination.

--- src/pages/Bakanes/index.tsx
+++ src/pages/Bakanes/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { Search } from "lucide-react";
 import CreateCategoryDrawer from "../../components/CreateCategoryDrawer/CreateCategoryDrawer";
 import "./Bakanes.css";
 
@@ -93,14 +92,28 @@ export default function Bakanes() {
       <div className="bakanes-toolbar">
         {/* Search */}
         <div className="bakanes-search-container">
-          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-600/70 w-4 h-4" />
-          <input
-            type="text"
-            placeholder="Buscar"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="bakanes-search-input"
-          />
+          <div className="bakanes-search-input-wrapper">
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="search-icon-svg"
+            >
+              <path
+                d="M8.33333 7.33333H7.80667L7.62 7.15333C8.27333 6.39333 8.66667 5.40667 8.66667 4.33333C8.66667 1.94 6.72667 0 4.33333 0C1.94 0 0 1.94 0 4.33333C0 6.72667 1.94 8.66667 4.33333 8.66667C5.40667 8.66667 6.39333 8.27333 7.15333 7.62L7.33333 7.80667V8.33333L10.6667 11.66L11.66 10.6667L8.33333 7.33333ZM4.33333 7.33333C2.67333 7.33333 1.33333 5.99333 1.33333 4.33333C1.33333 2.67333 2.67333 1.33333 4.33333 1.33333C5.99333 1.33333 7.33333 2.67333 7.33333 4.33333C7.33333 5.99333 5.99333 7.33333 4.33333 7.33333Z"
+                fill="#1840B2"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Buscar"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="bakanes-search-input"
+            />
+          </div>
         </div>
 
         {/* Filters */}
